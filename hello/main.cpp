438b3cce881f25f9a7f790cb0b95fbcf233c51a9
Youngmin Kim <ymkim@huvitz.com>
2020-07-07 16:51:43 +0900

    insert myfilter to the pipeline. (set GST_PLUGIN_PATH=/tmp/gstreamer-1.0)

--- hello/main.cpp
+++ hello/main.cpp
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 
 	const char * desc;
 #ifdef __RK3399__
-	desc = "rkisp device=/dev/video0 io-mode=1 analyzer=1 enable-3a=1 ! video/x-raw,format=NV12,width=640,height=480,framerate=30/1 ! kmssink render-rectangle=\"<400, 100, 320, 240>\"";
+	desc = "rkisp device=/dev/video0 io-mode=1 analyzer=1 enable-3a=1 ! video/x-raw,format=NV12,width=320,height=240,framerate=30/1 ! myfilter int=128 silent=true ! kmssink render-rectangle=\"<400,100,320,240>\"";
 #else
 	desc = "videotestsrc ! video/x-raw,format=NV12,width=640,height=480,framerate=30/1 ! videoconvert ! autovideosink";
 #endif
