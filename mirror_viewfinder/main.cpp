df6b5ac5619fff6adadf6b71035d193470a06a79
Youngmin Kim <kimyoungmin@gmail.com>
2020-07-22 11:26:21 +0900

    rk3399 abstractvideosurface cameraviewfindersettings

--- mirror_viewfinder/main.cpp
+++ mirror_viewfinder/main.cpp
@@ -4,6 +4,7 @@
 #include "sharedviewfinder.h"
 
 #include <QCameraInfo>
+#include <QCameraViewfinderSettings>
 
 int main(int argc, char *argv[])
 {
@@ -19,6 +20,30 @@ int main(int argc, char *argv[])
 	QList<QCameraInfo> cameras = QCameraInfo::availableCameras();
 	QCamera camera(cameras[0]);
 	camera.setViewfinder(&viewfinder);
+
+#ifdef __RK3399__
+	QCameraViewfinderSettings viewfinderSettings = camera.viewfinderSettings();
+
+	qDebug("DBG default viewfinderSettings : %f~%f, %d:%d, %d*%d, %d",
+		   viewfinderSettings.minimumFrameRate(),
+		   viewfinderSettings.maximumFrameRate(),
+		   viewfinderSettings.pixelAspectRatio().width(),
+		   viewfinderSettings.pixelAspectRatio().height(),
+		   viewfinderSettings.resolution().width(),
+		   viewfinderSettings.resolution().height(),
+		   viewfinderSettings.pixelFormat() );
+
+#define RESOLUTION_X 800
+#define RESOLUTION_Y 480
+	qDebug("DBG set viewfinderSettings 1~30, 16:9, %dx%d, NV12", RESOLUTION_X, RESOLUTION_Y);
+	viewfinderSettings.setResolution(RESOLUTION_X, RESOLUTION_Y);
+	viewfinderSettings.setPixelAspectRatio(16, 9);
+	viewfinderSettings.setMinimumFrameRate(1.0);
+	viewfinderSettings.setMaximumFrameRate(30.0);
+	viewfinderSettings.setPixelFormat(QVideoFrame::Format_NV12);
+
+	camera.setViewfinderSettings(viewfinderSettings);
+#endif
 
 	camera.start();
 
